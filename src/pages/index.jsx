8924617bcdb952d49e9253a958c4d07f1c6ef71e
emoraisdev <evertonsmorais@gmail.com>
2025-09-29 23:46:54 -0300

    feat: Criando Exemplo de Estilos

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Inicio(){
+
+    return (
+        <div>
+            <h1>Fundamentos Next.js & React</h1>
+            <h2>Aprendendo este framework</h2>
+        </div>
+    )
+}
